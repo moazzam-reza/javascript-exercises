666bc7161a89401d175b3e5e57a8e1dd9b3de575
Moazzam Reza <moazzamreza.33@gmail.com>
2025-02-18 18:30:49 -0500

    js exercises done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let ageToName = {};
 
+    for (let obj of arr) {
+        let yearOfDeath;
+
+        if (obj.yearOfDeath === undefined) {
+            yearOfDeath = new Date().getFullYear();
+        } 
+        else {
+            yearOfDeath = obj.yearOfDeath;
+        }
+
+        ageToName[yearOfDeath - obj.yearOfBirth] = obj.name;
+    }
+
+    let orderedAges = Object.keys(ageToName).sort((a, b) => +a - +b);
+
+    return arr.find((obj) => obj.name == ageToName[orderedAges.at(-1)]);
 };
 
 // Do not edit below this line
